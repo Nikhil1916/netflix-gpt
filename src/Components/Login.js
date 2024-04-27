6ef475f6872070b4264623a6fc5ce3426c00731f
nikhil <nikhilchawla9013@gmail.com>
2024-04-27 23:58:06 +0530

    fixed css of login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,10 +8,11 @@ const Login = () => {
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
          alt="login-bg" />
         </div>
-        <form className="absolute top-[30%] left-[38%] w-3/12 bg-black flex flex-col p-4">
-            <input type="text" placeholder="Email Address" className="p-2 m-2" />
-            <input type="password" placeholder="Password" className="p-2 m-2" />
-            <button className="p-4 m-4 bg-black text-white">Sign In</button>
+        <form className="absolute top-[20%] left-[38%] w-3/12 bg-black flex flex-col p-12 text-white bg-opacity-80 rounded-lg">
+            <h1 className="font-bold text-3xl py-4">Sign In</h1>
+            <input type="text" placeholder="Email Address" className="p-4 my-4 w-full bg-gray-800 rounded-lg" />
+            <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-800 rounded-lg" />
+            <button className="py-4 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
         </form>
         </div>
     )
