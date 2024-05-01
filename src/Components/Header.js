bee41c27b4016ec64d6405c240dbcfedd9777950
nikhil <nikhilchawla9013@gmail.com>
2024-05-01 15:12:43 +0530

    added font awesome

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,14 @@
 import { signOut } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../Utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 const Header = () => {
     const navigate = useNavigate();
     const user = useSelector((store)=>store.user);
-    console.log(user);
+    const [isDropdownOpen, setIsDropDown] = useState(false);
     const handleSignOut = () => {
         signOut(auth)
           .then(() => {
@@ -19,6 +21,10 @@ const Header = () => {
           });
           
     }
+
+    const toggleDropdown = () => {
+      setIsDropDown(!isDropdownOpen);
+    }
     return (
         <div className="header absolute w-full h-24 px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
             <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
@@ -26,9 +32,13 @@ const Header = () => {
             />
             {
              user && 
-            <div className="flex p-2 gap-2">
+            <div className="flex items-center p-2 gap-2 justify-center" onMouseLeave={toggleDropdown}>
             <img src="https://occ-0-3240-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" className="w-12 h-12" alt="user-icon"/>
-            <button className="font-bold text-white" onClick={handleSignOut}>Sign Out</button>
+            {
+              !isDropdownOpen ? <FontAwesomeIcon icon={faAngleDown} className="text-white" onMouseEnter={toggleDropdown} /> : <FontAwesomeIcon icon={faAngleUp} className="text-white" />
+            }
+              
+            {isDropdownOpen && <div className="absolute top-20 mr-10 bg-black p-3 w-30"> <button className="font-bold text-white" onClick={handleSignOut}>Sign Out</button> </div>}
             </div>
             }
         </div>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -74,7 +74,7 @@ const Login = () => {
         <Header/>
         <div className="absolute">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-         alt="login-bg" className="login-bg" />
+         alt="login-bg" className="login-bg w-[100%] h-[100%]" />
         </div>
         <form className="absolute top-[20%] left-[38%] w-3/12 bg-black flex flex-col p-12 text-white bg-opacity-80 rounded-lg">
             <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
