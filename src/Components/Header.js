9cd930b44806d5b5b13a777c159b47a0ed1ee1e0
nikhil <nikhilchawla9013@gmail.com>
2024-05-02 13:03:37 +0530

    add logout dropdown

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -26,23 +26,49 @@ const Header = () => {
       setIsDropDown(!isDropdownOpen);
     }
     return (
-        <div className="header absolute w-full h-24 px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
-            <img src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-            alt="logo" className="h-24"
+      <div className="header absolute w-full h-24 px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
+        <img
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="logo"
+          className="h-24"
+        />
+        {user && (
+          <div
+            className="flex items-center p-2 gap-2 justify-center cursor-pointer"
+            onMouseLeave={toggleDropdown}
+          >
+            <img
+              src="https://occ-0-3240-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+              className="w-12 h-12"
+              alt="user-icon"
             />
-            {
-             user && 
-            <div className="flex items-center p-2 gap-2 justify-center" onMouseLeave={toggleDropdown}>
-            <img src="https://occ-0-3240-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" className="w-12 h-12" alt="user-icon"/>
-            {
-              !isDropdownOpen ? <FontAwesomeIcon icon={faAngleDown} className="text-white" onMouseEnter={toggleDropdown} /> : <FontAwesomeIcon icon={faAngleUp} className="text-white" />
-            }
+            {!isDropdownOpen ? (
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="text-white"
+                onMouseEnter={toggleDropdown}
+              />
+            ) : (
+              <FontAwesomeIcon icon={faAngleUp} className="text-white" />
+            )}
+
+            {isDropdownOpen && (
+              <div className="absolute top-20 mr-10 bg-black p-3 w-30">
+              <div>
+                <button
+                  className="font-bold text-white"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </button>
+              </div>
               
-            {isDropdownOpen && <div className="absolute top-20 mr-10 bg-black p-3 w-30"> <button className="font-bold text-white" onClick={handleSignOut}>Sign Out</button> </div>}
-            </div>
-            }
-        </div>
-    )
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default Header;
