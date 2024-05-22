599445ce41e7df35ebb4644bfb1fc06419f585d9
nikhil <nikhilchawla9013@gmail.com>
2024-05-22 23:08:41 +0530

    redeploy via git

--- src/Components/GptMovieSuggestions.js
+++ src/Components/GptMovieSuggestions.js
@@ -7,14 +7,11 @@ const GptMovieSuggestions = () => {
   const gptMovies = useSelector(store=>store.movies);
   const {gptSearchMovieSuggestions,gptMovieName} = gptMovies;
   return (
-    // <div className='flex justify-center mt-2'>
     <div className='p-4 bg-opacity-70 text-white mt-2'>
         {
           gptMovieName?.length > 0 ? gptMovieName?.map((movie,i)=> gptSearchMovieSuggestions?.[i]?.length ? <MovieList key={movie} title={movie} movies={gptSearchMovieSuggestions?.[i]} /> : null) : gptMovieName?.length===0 && [1,2,3,4]?.map((i)=><MovieList key={i} movies={[]} title={"Loading...."} />)
         }
     </div>
-
-    // </div>
   )
 }
 
